db2d333960b1afae0c055ac10445362cabea448a
praisethemoon <doit@praisethemoon.org>
2016-12-24 11:15:49 +0100

    ast++

--- source/parser.h
+++ source/parser.h
@@ -7,21 +7,49 @@
 #include "mugen.h"
 #include "engine.h"
 
+/*
+ * Helper macros,
+ * Write once use, use everywhere.
+ */
+
+/* used to allocate all kind of structures */
 #define NEW(type) type* new_##type(){ return (type*)dmt_calloc(1, sizeof(type)); }
 
+/* is(object, type) checks if object.typeField == type */
+#define IS(o, t) (o.typeField == ENUM_##t)
+
+/* all enums has the same structures */
+#define ENUM(t) ENUM_##t
+
 typedef enum FunctionBodyType{
-    FBT_EXPR = 0,
-    FBT_BODY = 1
+    ENUM(Expr) = 0,
+    ENUM(Block) = 1
 }FunctionBodyType;
 
 typedef enum TypeExprType{
-    TET_BASE,
-    TET_FN,
-    TET_TEMPLATE,
-    TET_ARRAY,
-    TET_TUPLE;
-    TET_INTERFACE,
+    ENUM(TypeBaseExpr),
+    ENUM(TypeFnExpr),
+    ENUM(TypeTemplateExpr),
+    ENUM(TypeArrayExpr),
+    ENUM(TypeTupleExpr),
+    ENUM(TypeInterfaceExpr),
 }TypeExprType;
+
+typedef enum TypeBaseType{
+    TBT_BYTE,
+    TBT_CHAR,
+    TBT_BOOL,
+    TBT_INT_8,
+    TBT_UINT_8,
+    TBT_INT_16,
+    TBT_UINT_16,
+    TBT_INT_32,
+    TBT_UINT_32,
+    TBT_INT_64,
+    TBT_UINT_64,
+    TBT_FLOAT,
+    TBT_DOUBLE,
+}TypeBaseType
 
 struct ContextStruct {
 	wchar_t *ReturnValue;             /* In this template all rules return a string. */
@@ -63,7 +91,7 @@ typedef struct GlobalFunctionDecl{
     vec_t(struct VariableDecl*) params;
     
     struct TypeExpr* returnType;
-    enum FunctionBodyType bodyType;
+    enum FunctionBodyType typeField;
 }GlobalFunctionDecl;
 
 typedef struct InterfaceDecl{
@@ -94,7 +122,22 @@ typedef struct TypeExpr{
     };
 }TypeExpr;
 
+typedef struct TypeBaseExpr{
+    enum TypeBaseType type;
+}TypeBaseExpr;
 
+typedef struct TypeFnExpr{
+    vec_t(struct TypeExpr*) params;
+    vec_t(struct TypeExpr*) returns;
+}TypeFnExpr;
+
+typedef struct TypeArrayExpr{
+    struct TypeExpr* type;
+}TypeArrayExpr;
+
+typedef struct TypeTupleExpr{
+    
+};
 
 //void parseAst(struct TokenStruct *Token, struct ContextStruct *context);
 
