65c3b2abf6901b5ba419e9d00314dbfb41920b26
praisethemoon <doit@praisethemoon.org>
2016-12-24 10:28:36 +0100

    ast++

--- source/parser.h
+++ source/parser.h
@@ -7,10 +7,21 @@
 #include "mugen.h"
 #include "engine.h"
 
+#define NEW(type) type* new_##type(){ return (type*)dmt_calloc(1, sizeof(type)); }
+
 typedef enum FunctionBodyType{
     FBT_EXPR = 0,
     FBT_BODY = 1
 }FunctionBodyType;
+
+typedef enum TypeExprType{
+    TET_BASE,
+    TET_FN,
+    TET_TEMPLATE,
+    TET_ARRAY,
+    TET_TUPLE;
+    TET_INTERFACE,
+}TypeExprType;
 
 struct ContextStruct {
 	wchar_t *ReturnValue;             /* In this template all rules return a string. */
@@ -58,15 +69,30 @@ typedef struct GlobalFunctionDecl{
 typedef struct InterfaceDecl{
     struct InterfaceHeader* header;
     
+    vec_t(struct OperatorDecl) operators;
     vec_t(struct InterfaceMethod*) methods;
 }InterfaceDecl;
 
 typedef struct ClassDecl{
     struct ClassHeader* header;
     
+    int isImmutable;
+    
     vec_t(struct ClassMethod*) methods;
+    vec_t(struct OperatorDecl) operators;
     vec_t(struct ClassAttributes*) attributes;
 }ClassDecl;
+
+
+typedef struct TypeExpr{
+    TypeExprType type;
+    union{
+        struct TypeBaseExpr* base;
+        struct TypeFnExpr* fn;
+        struct TypeArrayExpr* array;
+        struct TypeTupleExpr* tuple
+    };
+}TypeExpr;
 
 
 
