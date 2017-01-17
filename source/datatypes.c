0766c37efcc0edaf7898ce06ac21b4cd3a91f02f
praisethemoon <doit@praisethemoon.org>
2017-01-17 22:47:36 +0100

    ast++

---
+++ source/datatypes.c
@@ -0,0 +1,12 @@
+#include "datatypes.h"
+
+Program* createProgram() {
+	Program *program = dmt_calloc(1, sizeof(Program));
+	vec_init(&program->imports);
+	vec_init(&program->externMethods);
+	vec_init(&program->variables);
+	vec_init(&program->datatypes);
+	vec_init(&program->functions);
+
+	return program;
+}
